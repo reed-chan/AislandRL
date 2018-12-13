4b05ccd33d328c9f9d3e6da6d521b6b378c2f96b
reed-chan <xbc@live.jp>
2018-12-13 21:46:15 +0800

    merge

--- src/glyph.js
+++ src/glyph.js
@@ -1,12 +1,9 @@
-Game.Glyph = function(properties) {
-    // Instantiate properties to default if they weren't passed
-    properties = properties || {};
-    this._char = properties['character'] || ' ';
-    this._foreground = properties['foreground'] || 'white';
-    this._background = properties['background'] || 'black';
+Game.Glyph = function(chr, foreground, background) {
+    this._char = chr || ' ';
+    this._foreground = foreground || 'white';
+    this._background = background || 'black';
 };
 
-// Create standard getters for glyphs
 Game.Glyph.prototype.getChar = function(){ 
     return this._char; 
 }
--- src/screens.js
+++ src/screens.js
@@ -17,106 +17,119 @@ Game.Screen.startScreen = {
 }
 Game.Screen.playScreen = {
 <<<<<<< HEAD
+<<<<<<< HEAD
     _map:null,
-    _player:null,
+    _centerX: 0,
+    _centerY: 0,
 
     enter:function() {
         var map = [];
-        var mapWidth = 80;
-        var mapHeight = 40;
+        var mapWidth = 100;
+        var mapHeight = 50;
         for(var x=0; x<mapWidth;x++) {
             map.push([]);
             for(var y=0;y<mapHeight;y++) {
-                map[x].push(Game.Tile.nullTile);
+                map[x].push(Game.Tile.nullTile)
             }
-        };
-        var generator = new ROT.Map.Digger(mapWidth,mapHeight);
+        }
+        var generator = new ROT.Map.Cellular(mapWidth,mapHeight);
+        generator.randomize(0.5);
+        var totalIterations = 3;
+        for (var i = 0; i < totalIterations - 1; i++) {
+            generator.create();
+        }
         generator.create(function(x,y,v) {
-            if (v === 0) {
+            if (v === 1) {
                 map[x][y] = Game.Tile.floorTile;
             } else {
                 map[x][y] = Game.Tile.wallTile;
             }
         });
         this._map = new Game.Map(map);
-        this._player = new Game.Entity(Game.PlayerTemplate);
-        var position = this._map.getRandomFloorPosition();
-        this._player.setX(position.x);
-        this._player.setY(position.y);        
     },
     exit:function() { },
     render:function(display) {
         var screenWidth = Game.getScreenWidth();
         var screenHeight = Game.getScreenHeight();
-        // Make sure the x-axis doesn't go to the left of the left bound
-        var topLeftX = Math.max(0, this._player.getX() - (screenWidth / 2));
-        // Make sure we still have enough space to fit an entire game screen
+        var topLeftX = Math.max(0, this._centerX - (screenWidth / 2));
         topLeftX = Math.min(topLeftX, this._map.getWidth() - screenWidth);
-        // Make sure the y-axis doesn't above the top bound
-        var topLeftY = Math.max(0, this._player.getY() - (screenHeight / 2));
-        // Make sure we still have enough space to fit an entire game screen
-        topLeftY = Math.min(topLeftY, this._map.getHeight() - screenHeight);
-        // Iterate through all visible map cells
+        var topLeftY = Math.max(0, this._centerY - (screenHeight / 2));
+        topLeftY = Math.min(topLeftY, this._map.getHeight() - screenHeight);        
         for (var x = topLeftX; x < topLeftX + screenWidth; x++) {
             for (var y = topLeftY; y < topLeftY + screenHeight; y++) {
                 // Fetch the glyph for the tile and render it to the screen
                 // at the offset position.
-                var tile = this._map.getTile(x, y);
+                var glyph = this._map.getTile(x, y).getGlyph();
                 display.draw(
                     x - topLeftX,
                     y - topLeftY,
-                    tile.getChar(), 
-                    tile.getForeground(), 
-                    tile.getBackground())
+                    glyph.getChar(), 
+                    glyph.getForeground(), 
+                    glyph.getBackground());
             }
-        }
-        // Render the player
+        };
         display.draw(
-            this._player.getX() - topLeftX, 
-            this._player.getY() - topLeftY,    
-            this._player.getChar(), 
-            this._player.getForeground(), 
-            this._player.getBackground()
-        );
+            this._centerX - topLeftX, 
+            this._centerY - topLeftY,
+            '@',
+            'white',
+            'black');
     },    
+<<<<<<< HEAD
 =======
     enter:function() { },
     exit:function() { },
     render:function(display) {
+=======
+    enter:function() { },
+    exit:function() { },
+    render:function(display) {
+>>>>>>> parent of 846e40a... Screen scoll
         display.drawText(3,5, "%c{red}%b{white}This game is so much fun!");
         display.drawText(4,6, "Press [Enter] to win, or [Esc] to lose!");
     },
 >>>>>>> parent of 846e40a... Screen scoll
+=======
+    move:function(dX, dY) {
+        this._centerX = Math.max(0,Math.min(this._map.getWidth() - 1, this._centerX + dX));
+        this._centerY = Math.max(0,Math.min(this._map.getHeight() - 1, this._centerY + dY));    
+    },
+>>>>>>> parent of cd5281f... 12/13
     handleInput:function(inputType, inputData) {
         if(inputType === 'keydown') {
-            // If enter is pressed, go to the win screen
-            // If escape is pressed, go to lose screen
-            if (inputData.keyCode === ROT.VK_RETURN) {
+            if(inputData.keyCode === ROT.KEYS.VK_RETURN) {
                 Game.switchScreen(Game.Screen.winScreen);
-            } else if (inputData.keyCode === ROT.VK_ESCAPE) {
+            }
+            else if (inputData.keyCode === ROT.KEYS.VK_ESCAPE) {
                 Game.switchScreen(Game.Screen.loseScreen);
             }
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
             // Movement
             if (inputData.keyCode === ROT.VK_LEFT) {
+=======
+
+            if (inputData.keyCode === ROT.KEYS.VK_LEFT) {
+>>>>>>> parent of cd5281f... 12/13
                 this.move(-1, 0);
-            } else if (inputData.keyCode === ROT.VK_RIGHT) {
+            } else if (inputData.keyCode === ROT.KEYS.VK_RIGHT) {
                 this.move(1, 0);
-            } else if (inputData.keyCode === ROT.VK_UP) {
+            } else if (inputData.keyCode === ROT.KEYS.VK_UP) {
                 this.move(0, -1);
-            } else if (inputData.keyCode === ROT.VK_DOWN) {
+            } else if (inputData.keyCode === ROT.KEYS.VK_DOWN) {
                 this.move(0, 1);
+<<<<<<< HEAD
             }
 =======
 >>>>>>> parent of 846e40a... Screen scoll
+=======
+>>>>>>> parent of 846e40a... Screen scoll
+=======
+            }   
+>>>>>>> parent of cd5281f... 12/13
         }
-    },
-    move:function(dX, dY) {
-        var newX = this._player.getX() + dX;
-        var newY = this._player.getY() + dY;
-        // Try to move to the new cell
-        this._player.tryMove(newX, newY, this._map);
-    }    
+    }
 }
 Game.Screen.winScreen = {
     enter:function() { },
