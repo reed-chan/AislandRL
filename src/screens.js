cd5281fc8935305bdeb0936ee7a5d40f633d46c1
reed-chan <xbc@live.jp>
2018-12-13 21:43:19 +0800

    12/13

--- src/screens.js
+++ src/screens.js
@@ -17,86 +17,94 @@ Game.Screen.startScreen = {
 }
 Game.Screen.playScreen = {
     _map:null,
-    _centerX: 0,
-    _centerY: 0,
+    _player:null,
 
     enter:function() {
         var map = [];
-        var mapWidth = 100;
-        var mapHeight = 50;
+        var mapWidth = 80;
+        var mapHeight = 40;
         for(var x=0; x<mapWidth;x++) {
             map.push([]);
             for(var y=0;y<mapHeight;y++) {
-                map[x].push(Game.Tile.nullTile)
+                map[x].push(Game.Tile.nullTile);
             }
-        }
-        var generator = new ROT.Map.Cellular(mapWidth,mapHeight);
-        generator.randomize(0.5);
-        var totalIterations = 3;
-        for (var i = 0; i < totalIterations - 1; i++) {
-            generator.create();
-        }
+        };
+        var generator = new ROT.Map.Digger(mapWidth,mapHeight);
         generator.create(function(x,y,v) {
-            if (v === 1) {
+            if (v === 0) {
                 map[x][y] = Game.Tile.floorTile;
             } else {
                 map[x][y] = Game.Tile.wallTile;
             }
         });
         this._map = new Game.Map(map);
+        this._player = new Game.Entity(Game.PlayerTemplate);
+        var position = this._map.getRandomFloorPosition();
+        this._player.setX(position.x);
+        this._player.setY(position.y);        
     },
     exit:function() { },
     render:function(display) {
         var screenWidth = Game.getScreenWidth();
         var screenHeight = Game.getScreenHeight();
-        var topLeftX = Math.max(0, this._centerX - (screenWidth / 2));
+        // Make sure the x-axis doesn't go to the left of the left bound
+        var topLeftX = Math.max(0, this._player.getX() - (screenWidth / 2));
+        // Make sure we still have enough space to fit an entire game screen
         topLeftX = Math.min(topLeftX, this._map.getWidth() - screenWidth);
-        var topLeftY = Math.max(0, this._centerY - (screenHeight / 2));
-        topLeftY = Math.min(topLeftY, this._map.getHeight() - screenHeight);        
+        // Make sure the y-axis doesn't above the top bound
+        var topLeftY = Math.max(0, this._player.getY() - (screenHeight / 2));
+        // Make sure we still have enough space to fit an entire game screen
+        topLeftY = Math.min(topLeftY, this._map.getHeight() - screenHeight);
+        // Iterate through all visible map cells
         for (var x = topLeftX; x < topLeftX + screenWidth; x++) {
             for (var y = topLeftY; y < topLeftY + screenHeight; y++) {
                 // Fetch the glyph for the tile and render it to the screen
                 // at the offset position.
-                var glyph = this._map.getTile(x, y).getGlyph();
+                var tile = this._map.getTile(x, y);
                 display.draw(
                     x - topLeftX,
                     y - topLeftY,
-                    glyph.getChar(), 
-                    glyph.getForeground(), 
-                    glyph.getBackground());
+                    tile.getChar(), 
+                    tile.getForeground(), 
+                    tile.getBackground())
             }
-        };
+        }
+        // Render the player
         display.draw(
-            this._centerX - topLeftX, 
-            this._centerY - topLeftY,
-            '@',
-            'white',
-            'black');
+            this._player.getX() - topLeftX, 
+            this._player.getY() - topLeftY,    
+            this._player.getChar(), 
+            this._player.getForeground(), 
+            this._player.getBackground()
+        );
     },    
-    move:function(dX, dY) {
-        this._centerX = Math.max(0,Math.min(this._map.getWidth() - 1, this._centerX + dX));
-        this._centerY = Math.max(0,Math.min(this._map.getHeight() - 1, this._centerY + dY));    
-    },
     handleInput:function(inputType, inputData) {
         if(inputType === 'keydown') {
-            if(inputData.keyCode === ROT.KEYS.VK_RETURN) {
+            // If enter is pressed, go to the win screen
+            // If escape is pressed, go to lose screen
+            if (inputData.keyCode === ROT.VK_RETURN) {
                 Game.switchScreen(Game.Screen.winScreen);
-            }
-            else if (inputData.keyCode === ROT.KEYS.VK_ESCAPE) {
+            } else if (inputData.keyCode === ROT.VK_ESCAPE) {
                 Game.switchScreen(Game.Screen.loseScreen);
             }
-
-            if (inputData.keyCode === ROT.KEYS.VK_LEFT) {
+            // Movement
+            if (inputData.keyCode === ROT.VK_LEFT) {
                 this.move(-1, 0);
-            } else if (inputData.keyCode === ROT.KEYS.VK_RIGHT) {
+            } else if (inputData.keyCode === ROT.VK_RIGHT) {
                 this.move(1, 0);
-            } else if (inputData.keyCode === ROT.KEYS.VK_UP) {
+            } else if (inputData.keyCode === ROT.VK_UP) {
                 this.move(0, -1);
-            } else if (inputData.keyCode === ROT.KEYS.VK_DOWN) {
+            } else if (inputData.keyCode === ROT.VK_DOWN) {
                 this.move(0, 1);
-            }   
+            }
         }
-    }
+    },
+    move:function(dX, dY) {
+        var newX = this._player.getX() + dX;
+        var newY = this._player.getY() + dY;
+        // Try to move to the new cell
+        this._player.tryMove(newX, newY, this._map);
+    }    
 }
 Game.Screen.winScreen = {
     enter:function() { },
--- src/tile.js
+++ src/tile.js
@@ -1,11 +1,31 @@
-Game.Tile = function(glyph) {
-    this._glyph = glyph;
+Game.Tile = function(properties) {
+    properties = properties || {};
+    // Call the Glyph constructor with our properties
+    Game.Glyph.call(this, properties);
+    // Set up the properties. We use false by default.
+    this._isWalkable = properties['isWalkable'] || false;
+    this._isDiggable = properties['isDiggable'] || false;
 };
+// Make tiles inherit all the functionality from glyphs
+Game.Tile.extend(Game.Glyph);
 
-Game.Tile.prototype.getGlyph = function() {
-    return this._glyph;
-};
+// Standard getters
+Game.Tile.prototype.isWalkable = function() {
+    return this._isWalkable;
+}
+Game.Tile.prototype.isDiggable = function() {
+    return this._isDiggable;
+}
 
-Game.Tile.nullTile = new Game.Tile(new Game.Glyph());
-Game.Tile.floorTile = new Game.Tile(new Game.Glyph('·', '#555'));
-Game.Tile.wallTile = new Game.Tile(new Game.Glyph('墙', '#8B7500'));
+
+Game.Tile.nullTile = new Game.Tile({});
+Game.Tile.floorTile = new Game.Tile({
+    character: '·',
+    foreground: '#555',
+    isWalkable: true        
+});
+Game.Tile.wallTile = new Game.Tile({
+    character: '墙',
+    foreground: '#8B7500',
+    isDiggable: true    
+});
